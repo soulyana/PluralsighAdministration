1a00d3bdb811def7960232f7d56609bdaf6fce53
Soulyana Lakew <soulyana3@gmail.com>
2018-12-11 19:38:47 -0500

    installed react, react router and flux

--- gulpfile.js
+++ gulpfile.js
@@ -63,18 +63,18 @@ gulp.task('css', function() {
         .pipe(gulp.dest(config.paths.dist + '/css'));
 });
 
-gulp.lint('lint', function() {
+gulp.task('lint', function() {
     return gulp.src(config.paths.js)
-        .pipe(eslint({config: 'eslint.config.json'}))
-        .pipe(eslint.format());
+        .pipe(lint({config: 'eslint.config.json'}))
+        .pipe(lint.format());
 });
 
 //to watch files, notify gulp and reloads browser
 gulp.task('watch', function() {
     gulp.watch(config.paths.html, ['html']);
-    gulp.watch(config.paths.js, ['js']);
+    gulp.watch(config.paths.js, ['js', 'lint']);
 });
 
 //default task
-gulp.task('default', ['html', 'js', 'css', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
 
