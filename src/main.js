d7a0b8c47a0efe9c4408179513faf2de01ae4593
Soulyana Lakew <soulyana.lakew@accenturefederal.com>
2018-12-19 21:50:14 -0500

    added route configurations and bootstrapping

--- src/main.js
+++ src/main.js
@@ -1,40 +1,9 @@
-$ = jQuery = require('jquery'); //defines jQuery globally
+"use strict";
+
 var React = require('react');
-var Home = require('./components/homePage');
-var Authors = require('./components/authors/authorPage');
-var About = require('./components/about/aboutPage');
-var Header = require('./components/common/header');
+var Router = require('react-router');
+var routes = require('./routes');
 
-// workaround for using "use strict" by using an ify
-(function (win) {
-    "use strict";
-    var App = React.createClass({
-        render: function () {
-            var Child;
-
-            switch (this.props.route) {
-                case 'about': Child = About; break;
-                case 'authors': Child = Authors; break;
-                default: Child = Home;
-            }
-
-            return (
-                <div>
-                    <Header/>
-                    <Child/>
-                </div>
-            );
-        }
-    });
-
-    function render() {
-        var route = win.location.hash.substr(1);
-        React.render(<App route={route} />, document.getElementById('app'));
-    }
-
-    React.render(<Home />, document.getElementById('app'));
-
-    win.addEventListener('hashchange', render);
-    render();
-})(window);
-
+Router.run(routes, function(Handler) {
+    React.render(<Handler/>, document.getElementById('app'));
+});
