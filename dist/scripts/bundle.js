aeb5017e86d087aabc87030c69dfa15d0fb48494
Soulyana Lakew <soulyana.lakew@accenturefederal.com>
2018-12-20 20:17:28 -0500

    PropTypes

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -50411,29 +50411,42 @@ module.exports = App;
 //form inputs sits
 
 var React = require("react");
-var Input = require('../common/textinput');
-
+var Input = require("../common/textinput");
 
 var AuthorForm = React.createClass({displayName: "AuthorForm",
+  propTypes: {
+    author: React.PropTypes.object.isRequired,
+    onSave: React.PropTypes.func.isRequired,
+    onChange: React.PropTypes.func.isRequired,
+    errors: React.PropTypes.object
+  },
+  
   render: function() {
     return (
       React.createElement("form", null, 
         React.createElement("h1", null, "Manage Author"), 
         React.createElement(Input, {
-            name: "firstName", 
-            label: "First Name", 
-            value: this.props.author.firstName, 
-            onChange: this.props.onChange, 
-            error: this.props.errors.firstName}), 
+          name: "firstName", 
+          label: "First Name", 
+          value: this.props.author.firstName, 
+          onChange: this.props.onChange, 
+          error: this.props.errors.firstName}
+        ), 
 
         React.createElement(Input, {
-            name: "lastName", 
-            label: "Last Name", 
-            value: this.props.author.lastName, 
-            onChange: this.props.onChange, 
-            error: this.props.errors.lastName}), 
+          name: "lastName", 
+          label: "Last Name", 
+          value: this.props.author.lastName, 
+          onChange: this.props.onChange, 
+          error: this.props.errors.lastName}
+        ), 
 
-        React.createElement("input", {type: "submit", value: "Save", className: "btn btn-default", onClick: this.props.onSave})
+        React.createElement("input", {
+          type: "submit", 
+          value: "Save", 
+          className: "btn btn-default", 
+          onClick: this.props.onSave}
+        )
       )
     );
   }
--- src/components/authors/authorForm.js
+++ src/components/authors/authorForm.js
@@ -3,29 +3,42 @@
 //form inputs sits
 
 var React = require("react");
-var Input = require('../common/textinput');
-
+var Input = require("../common/textinput");
 
 var AuthorForm = React.createClass({
+  propTypes: {
+    author: React.PropTypes.object.isRequired,
+    onSave: React.PropTypes.func.isRequired,
+    onChange: React.PropTypes.func.isRequired,
+    errors: React.PropTypes.object
+  },
+  
   render: function() {
     return (
       <form>
         <h1>Manage Author</h1>
         <Input
-            name="firstName"
-            label="First Name"
-            value={this.props.author.firstName}
-            onChange={this.props.onChange} 
-            error={this.props.errors.firstName}/>
+          name="firstName"
+          label="First Name"
+          value={this.props.author.firstName}
+          onChange={this.props.onChange}
+          error={this.props.errors.firstName}
+        />
 
         <Input
-            name="lastName"
-            label="Last Name"
-            value={this.props.author.lastName}
-            onChange={this.props.onChange} 
-            error={this.props.errors.lastName}/>
+          name="lastName"
+          label="Last Name"
+          value={this.props.author.lastName}
+          onChange={this.props.onChange}
+          error={this.props.errors.lastName}
+        />
 
-        <input type="submit" value="Save" className="btn btn-default" onClick={this.props.onSave}/>
+        <input
+          type="submit"
+          value="Save"
+          className="btn btn-default"
+          onClick={this.props.onSave}
+        />
       </form>
     );
   }
