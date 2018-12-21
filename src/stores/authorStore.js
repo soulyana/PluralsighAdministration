a5e3937751239620ca15f83fcf3d488b6cb981cb
Soulyana Lakew <soulyana.lakew@accenturefederal.com>
2018-12-20 21:56:28 -0500

    Stores: Private Storage

--- src/stores/authorStore.js
+++ src/stores/authorStore.js
@@ -1,30 +1,43 @@
 "use strict";
 
-var Dispatcher = require('../dispatcher/appDispatcher');
-var ActionTypes = require('../constants/actionTypes');
-var EventEmitter = require('events').EventEmitter;
-var assign = require('object-assign');
-var CHANGE_EVENT = 'change';
+var Dispatcher = require("../dispatcher/appDispatcher");
+var ActionTypes = require("../constants/actionTypes");
+var EventEmitter = require("events").EventEmitter;
+var assign = require("object-assign");
+var _ = require("lodash");
+var CHANGE_EVENT = "change";
+
+var _authors = [];
 
 var AuthorStore = assign({}, EventEmitter.prototype, {
-    addChangeListener: function(callback) {
-        this.on(CHANGE_EVENT , callback);
-    },
+  addChangeListener: function(callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
 
-    removeChangeListener: function(callback) {
-        this.removeListener(CHANGE_EVENT , callback);
-    },
+  removeChangeListener: function(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  },
 
-    emitChange: function() {
-        this.emit(CHANGE_EVENT );
-    }
+  emitChange: function() {
+    this.emit(CHANGE_EVENT);
+  },
+
+  getAllAuthors: function() {
+    return _authors;
+  },
+
+  getAuthorById: function(id) {
+    return _.find(_authors, { id: id });
+  }
 });
 
-//private implementation detail 
-Dispatcher.register(function(action){
-    switch(action.actionTypes) {
-        
-    }
-}); 
+//private implementation detail
+Dispatcher.register(function(action) {
+  switch (action.actionTypes) {
+    case ActionTypes.CREATE_AUTHOR:
+      _authors.push(action.author);
+      AuthorStore.emitChange();
+  }
+});
 
 module.exports = AuthorStore;
